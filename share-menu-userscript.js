2eb7a9e371b0c925ba1e59635df4e74cca850583
Ing. Carlos Enrique <44654304+carlosepcc@users.noreply.github.com>
2025-03-04 09:45:07 -0500

    Update share-menu-userscript.js

--- share-menu-userscript.js
+++ share-menu-userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SIGA Share Menu Userscript
 // @namespace    http://tampermonkey.net/
-// @version      2.9.1
+// @version      2.9.2
 // @description  Este userscript añade botones para compartir el menú desde SIGA
 // @author       carlosepcc,jesusfvb
 // @match        https://alimentacion.uci.cu/
